69a8548a964b9eb774cffb5a1a12f0f9ebe96abf
fajariadi-id <fajariadi.id@gmail.com>
2021-07-21 22:00:23 +0700

    on progress: vuex todo manager -> filter limit todo

---
+++ vuex-todo-manager/src/components/Todos.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h3>Todos</h3>
+
+    <div class="todos">
+      <div class="todo" v-for="todo in getTodos" :key="todo.id">
+        {{ todo.title }}
+        <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'Todos',
+  methods: {
+    ...mapActions(['fetchTodos', 'deleteTodo']),
+  },
+  computed: mapGetters(['getTodos']),
+  created() {
+    this.fetchTodos();
+  },
+};
+</script>
+
+<style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
---
+++ vuex-todo-manager/src/store/modules/todos.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+const state = {
+  todos: [],
+};
+
+const getters = {
+  getTodos: (state) => state.todos,
+};
+
+const actions = {
+  async fetchTodos({ commit }) {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/todos');
+
+    commit('setTodos', res.data);
+  },
+  async addTodo({ commit }, title) {
+    const res = await axios.post('https://jsonplaceholder.typicode.com/todos', {
+      title,
+      completed: false,
+    });
+
+    commit('newTodo', res.data);
+  },
+  async deleteTodo({ commit }, id) {
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+
+    commit('removeTodo', id);
+  },
+  async filterTodo({ commit }, e) {
+    console.log(+e.target.value);
+  },
+};
+
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos),
+  newTodo: (state, todo) => state.todos.unshift(todo),
+  removeTodo: (state, id) =>
+    (state.todos = state.todos.filter((todo) => todo.id !== id)),
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
