7a33a20cdec675654e7f77e1e2470c115560057f
fajariadi-id <fajariadi.id@gmail.com>
2021-07-24 06:48:26 +0700

    random user cdn - ready to deploy

---
+++ project-display/src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="container">
+    <h1 class="heading">
+      mini projects - vue.js
+    </h1>
+
+    <div class="projects-container">
+      <Project />
+    </div>
+  </div>
+</template>
+
+<script>
+import Project from './components/Project.vue';
+
+export default {
+  components: {
+    Project,
+  },
+};
+</script>
+
+<style lang="scss">
+@import './assets/scss/_variables.scss';
+
+/* GLOBAL */
+*,
+::after,
+::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+body {
+  font-family: $ff-primary;
+  background-color: $clr-white;
+  color: $clr-black;
+  line-height: 1.5;
+  font-size: 0.875rem;
+}
+
+ul {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+img {
+  display: block;
+  width: 100%;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  line-height: 1.25;
+  margin-bottom: 0.75rem;
+}
+
+h1 {
+  font-size: 3rem;
+}
+h2 {
+  font-size: 2rem;
+}
+h3 {
+  font-size: 1.25rem;
+}
+h4 {
+  font-size: 0.875rem;
+}
+
+p {
+  margin-bottom: 1.25rem;
+}
+
+.container {
+  min-height: 100vh;
+  width: 80vw;
+  margin: 0 auto;
+}
+
+/* CUSTOM */
+.heading {
+  font-weight: $fw-bold;
+  text-align: center;
+  color: #dedede;
+  font-size: 68px;
+  margin-top: 5rem;
+}
+</style>
---
+++ project-display/src/components/Project.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="project">
+    <img src="../assets/images/vuex-todo-manager.png" alt="" />
+  </div>
+</template>
+
+<script>
+// import {} from '../assets/images/vuex-todo-manager.png'
+
+export default {
+  name: 'Project',
+};
+</script>
+
+<style lang="scss" scoped>
+img {
+  box-shadow: 0 10px 15px rgba($color: #000000, $alpha: 0.1);
+}
+</style>
